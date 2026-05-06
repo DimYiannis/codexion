64ec655fceaee470c0ef5a6b13b7bf32c7f2a6bb
DimYiannis <johndim99@gmail.com>
2026-05-06 16:01:18 +0200

    one log for everything to avoid repetition

--- coders/log.c
+++ coders/log.c
@@ -12,65 +12,14 @@
 
 #include "header.h"
 
-void print_state(t_coder *coder)
-{
-  pthread_mutex_t printlock;
 
-  pthread_mutex_t_lock(&printlock);
-  printf("%d: %d %s\n",time coder->id, message);
-  pthread_mutex_t_unlock(&printlock);
-}
-
-
-void log_compiling(t_coder *coder)
+void log_event(t_coder *coder, char *msg)
 {
   long ms;
 
   ms = get_time_ms(coder->sim->start_time);
   pthread_mutex_t_lock(&coder->sim->print_mutex);
-  print("%ld %d is compiling\n");
+  printf("%ld %d %s\n", ms, coder->id, msg);
   pthread_mutex_t_unlock(&coder->sim->print_mutex);
-}
-
-void log_debugging()
-{
-  long ms;
-
-  ms = get_time_ms(coder->sim->start_time);
-  pthread_mutex_t_lock(&coder->sim->print_mutex);
-  print("%ld %d is compiling\n");
-  pthread_mutex_t_unlock(&coder->sim->print_mutex);
-
-}
-
-void log_refactoring()
-{
-  long ms;
-
-  ms = get_time_ms(coder->sim->start_time);
-  pthread_mutex_t_lock(&coder->sim->print_mutex);
-  print("%ld %d is compiling\n");
-  pthread_mutex_t_unlock(&coder->sim->print_mutex);
-
-}
-void log_taken_dongle()
-{
-  long ms;
-
-  ms = get_time_ms(coder->sim->start_time);
-  pthread_mutex_t_lock(&coder->sim->print_mutex);
-  print("%ld %d is compiling\n");
-  pthread_mutex_t_unlock(&coder->sim->print_mutex);
-
-}
-void log_burnout()
-{
-  long ms;
-
-  ms = get_time_ms(coder->sim->start_time);
-  pthread_mutex_t_lock(&coder->sim->print_mutex);
-  print("%ld %d is compiling\n");
-  pthread_mutex_t_unlock(&coder->sim->print_mutex);
-
 }
 
