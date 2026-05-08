0d964f4d3f105d7237aaff12fdc96b88c84d319b
DimYiannis <johndim99@gmail.com>
2026-05-08 10:46:56 +0200

    tranversing through coders with array indexing and either append or pop coders from the list, need to check the dereference

--- coders/fifo.c
+++ coders/fifo.c
@@ -16,17 +16,23 @@ init_queue(t_sim *sim)
 {
   t_queue queue;
   queue->coders = sim->coders;
-  queue->size = sim->args->num_of_coders;
+  queue->size = 0;
 }
 
-void dequeue(t_queue *queue, t_coder *coder)
+void enqueue(t_queue *queue, t_coder *coder)
 {
-
+  queue->coders[size] = coder;
+  queue->size++;
 }
 
 t_coder *dequeue(t_queue *queue)
 {
+  int i;
 
+  i = 1;
+  while (i < queue->size)
+    queue->coders[0] = queue->coders[i];
+  queue->size--;
 }
 
 
